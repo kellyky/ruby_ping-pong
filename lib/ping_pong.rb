7b4c92918d8da5fa4d5f90a6139b4c9c77b306c8
Kelly Young <kelpopko@gmail.com>
2022-03-08 09:31:00 -0600

    Rename variables

--- lib/ping_pong.rb
+++ lib/ping_pong.rb
@@ -1,10 +1,10 @@
 def ping_pong(number)
   (1..number).map do |num|  
-    ping = (num % 3 == 0)
-    pong = (num % 5 == 0)
-    ping && pong ? 'ping-pong'
-    : pong ? 'pong'
-    : ping ? 'ping'
+    divisible_by_three = (num % 3 == 0)
+    divisible_by_five = (num % 5 == 0)
+    divisible_by_three && divisible_by_five ? 'ping-pong'
+    : divisible_by_five ? 'pong'
+    : divisible_by_three ? 'ping'
     : num
   end
  
